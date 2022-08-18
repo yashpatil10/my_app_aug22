46885d6943bddb2ab3ecd00ab4e83713672a5620
yashwant patil <iamyashpatil10@gmail.com>
2022-08-18 15:40:45 +0530

    giving score

--- lib/main.dart
+++ lib/main.dart
@@ -20,15 +20,29 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [{'text':'Black','score':10}, 
+      {'text' :'Red','score': 5}, 
+      {'text':'Green','score':3},
+      {'text':'White','score':'1'} ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'answers': [
+      
+      {'text':'Rabbit', 'score':3},
+      {'text':'Snake', 'score':11},
+      {'text':'Elephant', 'score':5},
+      {'text':'Lion', 'score':9},
+      ],
     },
     {
       'questionText': 'Who\'s your favorite instructor?',
-      'answers': ['Max', 'Max', 'Max', 'Max'],
+      'answers': [
+        {'text':'Max', 'score':1},
+        {'text':'Max', 'score':1},
+        {'text':'Max', 'score':1},
+        {'text':'Max', 'score':1},
+      ],
     },
   ];
   var _questionIndex = 0;
